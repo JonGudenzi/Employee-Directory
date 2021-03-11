7365ec90f707c0218a05862bcdabd75650f3619e
JonGudenzi <jongudenzi@gmail.com>
2021-03-11 17:44:58 -0500

    create a header element - jumbotron

---
+++ employee-directory/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className="jumbotron">
+      <h1 className="display-3">Employee Directory</h1>
+      <p className="lead">Click on carrots to filter by heading or use the search box to narrow your results</p>
+      <p className="lead"></p>
+    </header>
+    );
+}
+
+export default Header;
